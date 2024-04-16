f81b5eb5dbe710dd5009254da976cad4745d11af
aimedidierm <aimedidiermugisha@gmail.com>
2024-04-16 23:36:13 +0200

    Login page UI

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:ihuriro/screens/theme/colors.dart';
+import 'package:ihuriro/screens/utils/text_utils.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,13 +12,125 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Text('hello'),
-        ],
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/bg.jpg'), fit: BoxFit.fill),
+        ),
+        alignment: Alignment.center,
+        child: Container(
+          height: 400,
+          width: double.infinity,
+          margin: const EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white),
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.black.withOpacity(0.1),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaY: 5, sigmaX: 5),
+              child: Padding(
+                padding: const EdgeInsets.all(25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Spacer(),
+                    Center(
+                      child: TextUtil(
+                        text: "Login",
+                        weight: true,
+                        size: 30,
+                      ),
+                    ),
+                    const Spacer(),
+                    TextUtil(
+                      text: "Email",
+                    ),
+                    Container(
+                      height: 35,
+                      decoration: const BoxDecoration(
+                          border:
+                              Border(bottom: BorderSide(color: Colors.white))),
+                      child: TextFormField(
+                        style: const TextStyle(color: Colors.white),
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(
+                            Icons.mail,
+                            color: Colors.white,
+                          ),
+                          fillColor: Colors.white,
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    TextUtil(
+                      text: "Password",
+                    ),
+                    Container(
+                      height: 35,
+                      decoration: const BoxDecoration(
+                          border:
+                              Border(bottom: BorderSide(color: Colors.white))),
+                      child: TextFormField(
+                        style: const TextStyle(color: Colors.white),
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(
+                            Icons.lock,
+                            color: Colors.white,
+                          ),
+                          fillColor: Colors.white,
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    Expanded(
+                      child: TextUtil(
+                        text: "FORGET PASSWORD",
+                        size: 12,
+                        weight: true,
+                      ),
+                    ),
+                    const Spacer(),
+                    Container(
+                      height: 40,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: primaryRed,
+                          borderRadius: BorderRadius.circular(30)),
+                      alignment: Alignment.center,
+                      child: TextUtil(
+                        text: "Log In",
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Spacer(),
+                    Center(
+                      child: TextUtil(
+                        text: "Don't have a account REGISTER",
+                        size: 12,
+                        weight: true,
+                      ),
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
